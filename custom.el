d0b584b6b3d4a84e963f1e72d10973bb09ece929
kena <171240650+kena093@users.noreply.github.com>
2025-01-15 22:36:26 +0900

    first commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org-journal doom-modeline doom-themes magit modus-themes))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "PlemolJP" :foundry "outline" :slant normal :weight regular :height 98 :width normal)))))
